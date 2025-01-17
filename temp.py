48271c335a5a3c98860ef093cb97e7cc5b6951da
Pritesh Jha <63769192+PriteshJha27@users.noreply.github.com>
2025-01-17 09:01:17 +0530

    Update temp.py

--- temp.py
+++ temp.py
@@ -1,1 +1,12 @@
-
+paths:
+  data:
+    table_metadata: "path/to/cstone_table_metadata.parquet"
+    column_metadata: "path/to/cstone_column_metadata.parquet"
+    cru_meta: "path/to/CRU_meta.xlsx"
+    proprietary_tables: "path/to/proprietary_tables.xlsx"
+    foreign_keys: "path/to/foreign_key.csv"
+  vectorstore:
+    save_path: "path/to/vectorstore/index.faiss"
+  model:
+    tokenizer: "path/to/tokenizer"
+    model: "path/to/model"
